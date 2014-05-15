49358b960e033312e9f432082564470c6d27e1db
Bryan Lai <blty_2312@hotmail.my>
2014-05-15 18:26:13 +0800

    [UPDATE] Set First Page to SignInPage

---
+++ ApolloWP/ApolloWP/Data/Form/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApolloWP.Data.Form
+{
+    public class LoginForm : AuthForm
+    {
+    }
+}
---
+++ ApolloWP/ApolloWP/Data/Form/RegistrationForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApolloWP.Data.Form
+{
+    public class RegistrationForm : AuthForm
+    {
+        public string Phone { get; set; }
+    }
+}
